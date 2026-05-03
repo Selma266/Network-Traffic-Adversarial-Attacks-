737809b0d68cc409e38478db62e596aaa5c067ce
Selma Rihani <sr5192968@gmail.com>
2026-05-03 18:03:28 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa: F401
-
-"""
-Constraints Guaranteed Evasion (CGE) algorithm.
-"""
-
-# noinspection PyPep8
-from .types import Validatable, CONSTR_DICT
-from .validation import Validation
